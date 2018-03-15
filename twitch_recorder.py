c6336b13b99f645ab6cdcc6ff5b920848d4aa09b
IFinners <IDFinners@Gmail.com>
2018-03-15 11:13:28 +0000

    Add check_and_rec() and retry functionality
    Solves failed recordings due to stream loading slowly or being delayed.

--- twitch_recorder.py
+++ twitch_recorder.py
@@ -9,21 +9,39 @@ import time
 import datetime
 import sys
 import pyautogui
+import psutil
 
 
-def record_stream(channel_name):
-    """Opens the channel_name Twitch stream in VLC and records it."""
-    # Open Terminal and click on it.
+def time_reached():
+    """Check if a time has been reached/passed."""
+    date = datetime.datetime.now()
+    hour = int(date.strftime('%-H'))
+    minutes = int(date.strftime('%-M'))
+    if hour > start_hour:
+        return True
+    elif hour == start_hour and minutes >= start_minutes:
+        return True
+
+
+def start_stream(channel_name):
+    """Opens the channel_name Twitch stream in VLC through the Terminal."""
+    # Open Terminal
     pyautogui.hotkey('ctrl', 'shift', 't')
     time.sleep(3)
-
-    # Input streamlink information
+    # Enter required Streamlink information
     pyautogui.typewrite('streamlink twitch.tv/{} best'.format(channel_name))
     pyautogui.press('enter')
-    time.sleep(10)
+    time.sleep(30)
 
-    # Record with VLC
-    pyautogui.hotkey('shift', 'r')
+
+def check_and_rec():
+    """Checks if VLC has sucessfully launched the stream then begins recording."""
+    for pid in psutil.pids():
+        p = psutil.Process(pid)
+        if p.name() == "vlc":
+            # Record with VLC using Hotkey
+            pyautogui.hotkey('shift', 'r')
+            return True
 
 
 channel = sys.argv[1]
@@ -37,14 +55,26 @@ print('Waiting for the scheduled time (' + str(start_hour).zfill(2) + ':'
       ' will begin then...')
 
 while True:
-    date = datetime.datetime.now()
-    hour = int(date.strftime('%-H'))
-    minutes = int(date.strftime('%-M'))
-    if hour == start_hour and minutes >= start_minutes:
+    if time_reached():
         break
     else:
         time.sleep(60)
 
-print('Starting and recording the stream.')
+print('Attempting to start and record the stream...')
 
-record_stream(channel)
+start_stream(channel)
+
+attempts = 1
+while True:
+    if check_and_rec():
+        print("Recording...")
+        break
+    elif attempts == 20:
+        print("The Stream has failed to start {} times "
+              "- Program shutting Down.".format(str(attempts)))
+        break
+    else:
+        print("The stream hasn't started sucessfully - Trying again in 30s.")
+        time.sleep(30)
+        attempts += 1
+        start_stream(channel)
